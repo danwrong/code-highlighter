1887ac2f8b79fc2f060ad51a803b49c957f4f5d3
danwebb <danwebb@a686c101-5f17-0410-80e1-b267258107f6>
2006-08-17 17:20:26 +0000

    initial import.
    git-svn-id: http://svn.danwebb.net/external/CodeHighlighter/trunk@30 a686c101-5f17-0410-80e1-b267258107f6

---
+++ ruby.js
@@ -0,0 +1,14 @@
+CodeHighlighter.addStyle("ruby",{
+	comment : {
+		exp  : /#[^\n]+/
+	},
+	brackets : {
+		exp  : /\(|\)/
+	},
+	string : {
+		exp  : /'[^']*'|"[^"]*"/
+	},
+	keywords : {
+		exp  : /\b(do|end|self|class|def|if|module|yield|then|else|for|until|unless|while|elsif|case|when|break|retry|redo|rescue|require|raise)\b/
+	}
+});
