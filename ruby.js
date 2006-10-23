206de3a97d77c63d7ca4823db2ed173616214c40
danwebb <danwebb@a686c101-5f17-0410-80e1-b267258107f6>
2006-10-23 15:05:57 +0000

    added symbol regexp to ruby grammar
    git-svn-id: http://svn.danwebb.net/external/CodeHighlighter/trunk@130 a686c101-5f17-0410-80e1-b267258107f6

--- ruby.js
+++ ruby.js
@@ -11,6 +11,7 @@ CodeHighlighter.addStyle("ruby",{
 	keywords : {
 		exp  : /\b(do|end|self|class|def|if|module|yield|then|else|for|until|unless|while|elsif|case|when|break|retry|redo|rescue|require|raise)\b/
 	},
+	/* Added by Shelly Fisher (shelly@agileevolved.com) */
 	symbol : {
 	  exp : /([^:])(:[A-Za-z0-9_!?]+)/
 	}
